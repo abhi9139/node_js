ed0d4ea7ca2c832e9dc8b689fe39a7207f0c5940
Abhishek Chand <abhishekchand@gmail.com>
2026-07-15 18:18:54 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
